1647828514c8d1778765d1b9a06a2fb870f9e330
dwibagus154 <dwibagus154@gmail.com>
2021-07-06 21:26:06 +0700

    masih belum lengkap

--- src/3_Custom-Message/src/object_detector.cpp
+++ src/3_Custom-Message/src/object_detector.cpp
@@ -1,1 +1,32 @@
 // TODO: implementasi object_detector.cpp
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include "std msgs/String.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "object detector");
+
+    ros::NodeHandle n;
+
+    ros::Publisher position_pub = n.advertise<custom_message::Position>("position", 1000);
+
+    ros::Rate loop_rate(60);
+
+    position.x = 640;
+    position.y = 480;
+
+    while (ros::ok())
+    {
+        std_msgs::String msg;
+        std::stringstream ss;
+        ss << "hello world ";
+        msg.data = ss.str();
+        ROS_INFO("%s", msg.data.c_str());
+
+        position_pub.publish(positon);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
