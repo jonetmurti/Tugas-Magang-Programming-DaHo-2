46e67fd33879196ee60c7d3f4b970e28fb00095b
Marcell43 <87226660+Marcell43@users.noreply.github.com>
2021-07-10 23:11:20 +0700

    Update publisher.cpp

--- src/1_Publisher-Subscriber/src/publisher.cpp
+++ src/1_Publisher-Subscriber/src/publisher.cpp
@@ -1,38 +1,23 @@
 #include "ros/ros.h"
-#include "std_msgs/Int32.h"
-
-class NumberPublisher {
-    private:
-        ros::NodeHandle n;
-        ros::Publisher chatter_pub;
-
-    public:
-        NumberPublisher() {
-            chatter_pub = n.advertise<std_msgs::Int32>("chatter", 1000);
-        }
-
-        void process(int data) {
-            std_msgs::Int32 msg;
-
-            msg.data = data;
-
-            chatter_pub.publish(msg);
-        }
-};
+#include "std_msgs/String.h"
 
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "publisher");
 
-    NumberPublisher np;
+    ros::NodeHandle nh;
 
-    ROS_INFO("publisher running...");
+    ros::Publisher topic_pub = nh.advertise<std_msgs::String>("Publisher", 10);
 
     ros::Rate loop_rate(60);
 
     while (ros::ok()) {
 
-        np.process(1);
+        std_msgs::String msg;
+        msg.data = "Hello World!";
+        
+        topic_pub.publish(msg);
+        ros::spinOnce();
 
         loop_rate.sleep();
     }
