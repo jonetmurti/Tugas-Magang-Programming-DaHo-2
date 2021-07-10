0a49b6505564a4f87f2aedddc1affd58d740fb44
Marcell43 <marcellino1702@gmail.com>
2021-07-10 22:03:56 +0700

    Add new feature: service-client

--- src/2_Service-Client/src/operator_service.cpp
+++ src/2_Service-Client/src/operator_service.cpp
@@ -1,1 +1,25 @@
-// TODO: implementasi operator_service.cpp
+#include "ros/ros.h"
+#include "intro/arithmetic.h"
+
+bool add(intro::arithmetic::Request  &req,
+         intro::arithmetic::Response &res)
+{
+    res.result = req.a + req.b;
+
+    return true;
+}
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "server");
+
+    ros::NodeHandle n;
+
+    ros::ServiceServer service = n.advertiseService("add_int", add);
+
+    ROS_INFO("server running...");
+
+    ros::spin();
+
+    return 0;
+}
