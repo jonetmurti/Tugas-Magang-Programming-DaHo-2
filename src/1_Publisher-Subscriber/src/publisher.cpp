226858520bb8b27904bf6c097e9f1945f1b9908e
Marcell43 <marcellino1702@gmail.com>
2021-07-10 21:57:18 +0700

    Add new feature: publisher

--- src/1_Publisher-Subscriber/src/publisher.cpp
+++ src/1_Publisher-Subscriber/src/publisher.cpp
@@ -1,1 +1,41 @@
-// TODO: implementasi publisher.cpp
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+class NumberPublisher {
+    private:
+        ros::NodeHandle n;
+        ros::Publisher chatter_pub;
+
+    public:
+        NumberPublisher() {
+            chatter_pub = n.advertise<std_msgs::Int32>("chatter", 1000);
+        }
+
+        void process(int data) {
+            std_msgs::Int32 msg;
+
+            msg.data = data;
+
+            chatter_pub.publish(msg);
+        }
+};
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "publisher");
+
+    NumberPublisher np;
+
+    ROS_INFO("publisher running...");
+
+    ros::Rate loop_rate(60);
+
+    while (ros::ok()) {
+
+        np.process(1);
+
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
--- src/1_Publisher-Subscriber/src/subscriber.cpp
+++ src/1_Publisher-Subscriber/src/subscriber.cpp
@@ -1,1 +1,46 @@
-// TODO: implementasi subscriber.cpp
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include <iostream>
+
+using namespace std;
+
+class NumberSubscriber {
+    private:
+        ros::NodeHandle n;
+        ros::Subscriber sub;
+        int counter;
+
+    public:
+        NumberSubscriber() {
+            counter = 0;
+            sub = n.subscribe<std_msgs::Int32>("chatter", 1000, &NumberSubscriber::chatterCallback, this);
+        }
+    
+        void chatterCallback(const std_msgs::Int32::ConstPtr& msg) {
+            counter += msg->data;
+        }
+
+        void print() {
+            cout << "counter: " << counter << endl;
+        }
+
+};
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "subscriber");
+
+    NumberSubscriber ns;
+
+    ROS_INFO("subscriber running...");
+
+    ros::Rate rate(60);
+
+    while(ros::ok()) {
+        ns.print();
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
